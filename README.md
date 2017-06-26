f42a0df76e21ae18d32dc78f917a58e8a89cf46d
Alex Henegar <alex.henegar19@gmail.com>
2017-06-26 12:13:28 -0400

    first commit up to adding todo item

---
+++ README.md
@@ -0,0 +1,2 @@
+# sinatra_todos
+A todo app made in Launch School program
---
+++ todo.rb
@@ -0,0 +1,69 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  # in a real application this "secret" should be much longer and stored in a
+  # environment variable. (in the .env file)
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  else
+  end
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# View a single todo list
+get "/lists/:id" do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+  erb :list, layout: :layout
+end
+
+post "/lists/:id" do
+  id = params[:id].to_i
+  todo_name = params[:todo_name]
+  session[:lists][id][:todos] << todo_name
+  redirect "/lists/#{id}"
+end
